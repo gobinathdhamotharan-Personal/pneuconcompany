d7575be1374b150fa36ccfc4a73245bce9e02cf8
Pavithra-dev-sys <pavi.dppg611@gmail.com>
2025-08-31 16:37:23 +0530

    newsletter

--- pneucon/src/app/home-page.component.ts
+++ pneucon/src/app/home-page.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { ServiceComponent } from './service/service.component';
 import { CertificateComponent } from './certificate/certificate.component';
+import { NewsletterComponent } from './newsletter/newsletter.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [HomeComponent, ServiceComponent, CertificateComponent],
+  imports: [HomeComponent, ServiceComponent, CertificateComponent,NewsletterComponent],
   template: `
     <app-home></app-home>
     <app-service></app-service>
+    <app-newsletter></app-newsletter>
     <app-certificate></app-certificate>
   `
 })
