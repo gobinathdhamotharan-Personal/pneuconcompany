12aff5c63a92984b51f82a3403f0eb410ef658a5
FreeLancer-chennai <jbbala06@gmail.com>
2025-09-07 20:07:05 +0530

    SimpleChanges

--- pneucon/src/app/footer/footer.component.ts
+++ pneucon/src/app/footer/footer.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Router, Event, NavigationEnd, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+ constructor(private router: Router) {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+      }
+    });
+  }
 }
